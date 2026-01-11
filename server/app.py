80432dbb41e5f196aac3bffdd4b495a9a7601b55
Mwendia1 <elijah.mwendia@student.moringaschool.com>
2026-01-11 19:24:44 +0300

    new changes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
+from models import db, Episode, Guest, Appearance
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lateshow.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
---
+++ server/seed.py
@@ -0,0 +1,19 @@
+from app import app
+from models import db, Episode, Guest, Appearance
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    e1 = Episode(date="1/11/99", number=1)
+    e2 = Episode(date="1/12/99", number=2)
+
+    g1 = Guest(name="Michael J. Fox", occupation="actor")
+    g2 = Guest(name="Sandra Bernhard", occupation="Comedian")
+    g3 = Guest(name="Tracey Ullman", occupation="television actress")
+
+    a1 = Appearance(rating=4, guest=g1, episode=e1)
+    a2 = Appearance(rating=5, guest=g3, episode=e2)
+
+    db.session.add_all([e1, e2, g1, g2, g3, a1, a2])
+    db.session.commit()
